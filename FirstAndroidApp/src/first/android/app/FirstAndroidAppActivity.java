4def2fea60d70236cb2b1ce35c9351a7e8be0dae
mhptsui <mhptsui@gmail.com>
2016-06-07 15:37:46 +0800

    Add files via upload

---
+++ FirstAndroidApp/src/first/android/app/FirstAndroidAppActivity.java
@@ -0,0 +1,13 @@
+package first.android.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstAndroidAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
